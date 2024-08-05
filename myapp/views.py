ceed83749cb8a372f5d4ff359cb94bb422ab5209
Chillyagi <chillys0326@gmail.com>
2024-08-06 07:46:48 +0800

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -43,50 +43,18 @@ for file in os.listdir("Docs"):
         text_path = "./Docs/" + file
         loader = TextLoader(text_path, encoding ="utf-8") #在這裡加上encoding參數否則python會報錯 目前只有txt有這個問題
         documents.extend(loader.load())
-'''
-class ChatGPT:  
-    
-
-    def __init__(self):
-        
-        self.messages = conversation
-        self.model = os.getenv("OPENAI_MODEL", default = "gpt-3.5-turbo")
-
-
-
-    def get_response(self, user_input):
-        conversation.append({"role": "user", "content": user_input})
-        
-
-        response = openai.ChatCompletion.create(
-	            model=self.model,
-                messages = self.messages
-
-                )
-
-        conversation.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
-        
-        print("AI回答內容：")        
-        print(response['choices'][0]['message']['content'].strip())
-
-
-        
-        return response['choices'][0]['message']['content'].strip()
-	
-'''
-#chatgpt = ChatGPT()
 
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000 , chunk_overlap=10)
 documents = text_splitter.split_documents(documents)
 vectordb = Chroma.from_documents(documents, embedding=OpenAIEmbeddings())
 #create the index in retriever interface
-retriever = vectordb.as_retriever(search_type="similarity",search_kwargs={"k":2})
+retriever = vectordb.as_retriever(search_type="similarity",search_kwargs={"k":3})
 
 
 
 
-qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0.2), retriever=retriever , verbose=False , chain_type="stuff")
+qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0.5), retriever=retriever , verbose=False , chain_type="stuff")
 chat_history= []
 
 @csrf_exempt
